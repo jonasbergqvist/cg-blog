f7f2b710057cf50edc94375ad0a3c194063ff663
jobe <Jonas.Bergqvist@episerver.com>
2023-06-05 12:19:02 +0200

    Working hmac

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,74 +7,63 @@ import Base64 from 'crypto-js/enc-base64';
 import md5 from 'crypto-js/md5';
 import hmacSHA256 from "crypto-js/hmac-sha256";
 
-function generateHMACKey (key, secretKey, body_str) {
-  let secret = Base64.parse(secretKey);
-  let method = "POST";
-  let target = `https://cg.optimizely.com/content/v2?auth=${process.env.NEXT_PUBLIC_CG_SINGLE_KEY}`;
-  let timestamp = (new Date()).getTime();
-  let nonce = Math.random().toString(36).substring(7);
-  let body = "";
-  body = body_str;
-  let bodybase64 = Base64.stringify(md5(body));
-  let message = key + method + target + timestamp + nonce + bodybase64;
-  let hmac = hmacSHA256(message, secret);
-  let base64hmac = Base64.stringify(hmac);
-  return "epi-hmac " + key + ":" + timestamp +":" + nonce + ":" + base64hmac;
+function generateHMAC(key: string, secretKey: string, url: URL | RequestInfo, init: RequestInit) {
+  const secret = Base64.parse(secretKey);
+  const method = init.method;
+  const target = new URL(url.toString()).pathname;
+  const timestamp = new Date().getTime();
+  const nonce = Math.random().toString(36).substring(7);
+  const body = init.body as any;
+  const body_b64 = Base64.stringify(md5(body));
+  const message = key + method + target + timestamp + nonce + body_b64;
+  const hmac = hmacSHA256(message, secret);
+  const base64hmac = Base64.stringify(hmac);
+  return key + ":" + timestamp + ":" + nonce + ":" + base64hmac;
 }
 
-const httpLink = new HttpLink({ uri: `https://cg.optimizely.com/content/v2?auth=${process.env.NEXT_PUBLIC_CG_SINGLE_KEY}` });
+const httpLink = new HttpLink({
+  uri: `https://cg.optimizely.com/content/v2`,
+  fetch: (uri, options) => {
+    const singleKey = process.env.NEXT_PUBLIC_CG_SINGLE_KEY
+    const appKey = process.env.NEXT_PUBLIC_CG_APP_KEY
+    const secret = process.env.NEXT_PUBLIC_CG_SECRET
+    const request = new Request(uri, options)
 
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-
-  const query = operation.query
-  const variables = operation.variables
-  let bodyString = JSON.stringify({
-    query,
-    variables
-  });
-
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      authorization: generateHMACKey(
-        process.env.NEXT_PUBLIC_CG_APP_KEY,
-        process.env.NEXT_PUBLIC_CG_SECRET,
-        bodyString
-      )
+    if (appKey && secret) {
+      const hmac = generateHMAC(appKey, secret, uri, options)
+      request.headers.set("Authorization", `epi-hmac ${hmac}`)
     }
-  }));
 
-  return forward(operation);
-})
+    else if (singleKey) {
+      request.headers.set("Authorization", `epi-single ${singleKey}`)
+    }
+
+    return fetch(request)
+  }
+});
 
 const activityMiddleware = new ApolloLink((operation, forward) => {
-  // add the recent-activity custom header to the headers
   operation.setContext(({ headers = {} }) => ({
     headers: {
       ...headers,
-      //'recent-activity': localStorage.getItem('lastOnlineTime') || null,
     }
   }));
 
   return forward(operation);
 })
 
-
-//const queryClient = new QueryClient()
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: from([
-    authMiddleware,
     activityMiddleware,
     httpLink
   ]),
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-  <ApolloProvider client={client}>
-    <Component {...pageProps} />
-  </ApolloProvider>
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
